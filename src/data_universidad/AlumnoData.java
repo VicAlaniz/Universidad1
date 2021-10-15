6b76ace179e0828189027ba2240dc2a6ef590ea0
ramalaniz <ramialanizfd@gmail.com>
2021-10-15 12:01:33 -0300

    Update AlumnoData.java

--- src/data_universidad/AlumnoData.java
+++ src/data_universidad/AlumnoData.java
@@ -2,28 +2,41 @@
 package data_universidad;
 
 import com.mysql.jdbc.Statement;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import universidad1.Alumno;
 import universidad1.Conectar;
 
 public class AlumnoData {
-    private Conectar conexion = null;
+    private Conectar conn = null;
 
-    public AlumnoData(Conectar conexion) throws SQLException {
-        this.conexion = (Conectar) conexion.getConexion();
+    public AlumnoData(Conectar conexion){
+        try {
+            this.conn = (Conectar) conexion.getConexion();
+        } catch (SQLException ex) {
+            Logger.getLogger(AlumnoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public void guardarAlumno(Alumno alum) throws SQLException{
-        String query = "INSERT INTO alumno()VALUES (?,?,?,?,?,)";
+    public void guardarAlumno(Alumno alum){
+        String query = "INSERT INTO alumno(apellido, nombre, fechaNac, legajo, activo)VALUES (?,?,?,?,?,)";
         
-        {
-            PreparedStatement ps = conexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        try{
+            PreparedStatement ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, alum.getNombre());
             ps.setString(2, alum.getApellido());
+            ps.setDate(3, Date.valueOf(alum.getFechaNac()));
+            ps.setInt(4, alum.getLegajo());
+            ps.setBoolean(5, alum.isActivo());
             
+            ResultSet rst = ps.executeQuery();
+        } catch (SQLException ex){
+            Logger.getLogger(AlumnoData.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }  
+    }
     
 }
